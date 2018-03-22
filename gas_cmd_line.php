f1ca1961f660265120e7e852b28c46ee819f04b9
Iain Dooley <iain@workingsoftware.com.au>
2018-03-22 15:12:06 +1100

    Delete requests for files that don't exist

--- gas_cmd_line.php
+++ gas_cmd_line.php
@@ -31,7 +31,8 @@
            preg_match("/.*Processed Notification.*/",$resp)||
            preg_match("/.*checkCCBBLib.*/",$resp)||
            preg_match("/.*Roger That.*/",$resp)||
-           preg_match("/.*Sorry, unable to open the file at this time.*/",$resp)
+           preg_match("/.*Sorry, unable to open the file at this time.*/",$resp)||
+           preg_match("/.*Sorry, the file you have requested does not exist.*/",$resp)
           )
         {
             echo "deleting".PHP_EOL;
