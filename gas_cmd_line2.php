0a84a534e342fc2b2ae434de29eca1466cc0d09d
root <root@ip-10-0-12-136.ap-southeast-2.compute.internal>
2018-08-16 15:38:38 +1000

    Base64 decode job data before echoing for debug

--- gas_cmd_line2.php
+++ gas_cmd_line2.php
@@ -9,7 +9,7 @@
         echo "reserved".PHP_EOL;
         $p->bury($job);
         $obj = json_decode(base64_decode($job->getData()));
-echo $job->getData().PHP_EOL;
+echo base64_decode($job->getData()).PHP_EOL;
         $success = true;
         $post_fields = $obj->post;
         echo "forward to: ".$obj->url.PHP_EOL;
