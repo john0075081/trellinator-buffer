c644ab8f59b0a6c3c3f67f3945d3857794d0a20e
root <root@ip-10-0-12-136.ap-southeast-2.compute.internal>
2018-10-25 18:45:27 +1100

    Removed condition where a blocked script id would cause a data block to be written to a tube with an incorrect script id name

--- gas_putter.js
+++ gas_putter.js
@@ -26,17 +26,15 @@ http.createServer(function (req, res) {
             ///forward-gas-service-test/AKfycbzxvwNqwgXXnJEcs0ICFEAZyY67o9P2zSIPjH4mGG4oOEGYoWw
             var parts = /^\/.+\/(.+)$/.exec(req.url);
             var script_id = parts[1];
+            var timeStamp = Math.floor(Date.now() / 20000);//20 second window
+            var use_tube = "trellinator-"+script_id+"-"+timeStamp;
             var new_url = "https://script.google.com/macros/s/"+script_id+"/exec";
+            var data = Buffer.from(JSON.stringify({url: new_url,post: post})).toString('base64');
             
             if([
                 "COPY THIS LINE AND PASTE IN A SCRIPT ID TO BLOCK",
                 "END BLOCKED SCRIPT IDS"].indexOf(script_id) == -1)
             {
-                //putJob(script_id,Buffer.from(JSON.stringify({url: new_url,post: post})).toString('base64'));
-                var data = Buffer.from(JSON.stringify({url: new_url,post: post})).toString('base64');
-                var timeStamp = Math.floor(Date.now() / 20000);//20 second window
-                var use_tube = "trellinator-"+script_id+"-"+timeStamp;
-            
                 client.use(use_tube).onSuccess(function(tube)
                 {
                     client.put(this.dataAsy).onSuccess(function()
